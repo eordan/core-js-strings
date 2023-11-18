6aa3b9c2be65fe6223942c3359199072aae2da72
eordan <pistsouandrey@gmail.com>
2023-11-18 15:51:54 +0300

    feat: solve isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -322,8 +322,15 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const clearedStr = str
+    .replaceAll('!', '')
+    .replaceAll('?', '')
+    .replaceAll(',', '')
+    .replaceAll(' ', '')
+    .toLowerCase();
+  const reversedStr = [...clearedStr].reverse().join('');
+  return clearedStr === reversedStr;
 }
 
 /**
