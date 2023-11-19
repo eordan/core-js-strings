315e8c95d426227e0c16509bad519db3e74b1199
eordan <pistsouandrey@gmail.com>
2023-11-19 10:36:54 +0300

    feat: solve countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -307,8 +307,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = str.match(/[aeiouy]/gi);
+  return vowels === null ? 0 : vowels.length;
 }
 
 /**
